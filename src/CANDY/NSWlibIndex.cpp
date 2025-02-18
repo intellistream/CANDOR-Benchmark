6f46b7186d5307fb3cc30c21189bdb86d93f699c
AmuroEita <1071307515@qq.com>
2025-02-18 21:55:16 +0800

    clean

--- src/CANDY/NSWlibIndex.cpp
+++ src/CANDY/NSWlibIndex.cpp
@@ -16,16 +16,37 @@ bool CANDY::NSWlibIndex::setConfig(INTELLI::ConfigMapPtr cfg) {
   maxElements = cfg->tryI64("maxElements", 10000, true);
   metricType = cfg->tryString("metricType", "IP", true);
   
-  index.init<double>(metricType, vecDim, maxElements, M, ef);
+  if (metricType == "L2") {
+    space = std::make_unique<hnswlib::L2Space>(vecDim);
+  } else if (metricType == "IP") {
+    space = std::make_unique<hnswlib::InnerProductSpace>(vecDim);
+  } else {
+    throw std::invalid_argument("Unsupported metric type");
+  }
+  index = std::make_unique<hnswlib::HierarchicalNSW<float>>(space.get(), maxElements, M, ef);
 
   return true;
 }
 
 bool CANDY::NSWlibIndex::insertTensor(torch::Tensor &t) {
-  index.insertTensor(t);
+  if (!index) throw std::runtime_error("NSWlib_HNSW not initialized");
+  auto vec = t.to(torch::kCPU).contiguous();
+  index->addPoint(vec.data_ptr<float>(), count++);
   return true;
 }
-std::vector<torch::Tensor> CANDY::NSWlibIndex::searchTensor(torch::Tensor &q,
-                                                                int64_t k) {
-  return index.searchTensor(q, k);
+
+std::vector<torch::Tensor> CANDY::NSWlibIndex::searchTensor(torch::Tensor &qt,
+                                                              int64_t k) {
+  if (!index) throw std::runtime_error("NSWlibHNSW not initialized");
+  auto q = qt.to(torch::kCPU).contiguous();
+  auto res = index->searchKnn(q.data_ptr<float>(), k);
+
+  std::vector<torch::Tensor> resT;
+  while (!res.empty()) {
+    char* data_ptr = static_cast<char*>(index->getDataByInternalId(res.top().second));
+    torch::Tensor t = torch::from_blob(data_ptr, {vecDim}, torch::kFloat32).clone();
+    resT.push_back(t);
+    res.pop();
+  }
+  return resT;
 }
